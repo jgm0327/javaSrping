48bbf699c0cd63295e22c38543f8157937ebbd3c
jgm0327 <youyou6349@gmail.com>
2022-05-24 23:11:22 +0900

    setting

---
+++ src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -0,0 +1,42 @@
+package com.fastcampus.programming.dmaker.controller;
+
+import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
+import com.fastcampus.programming.dmaker.service.DMakerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 사용자의 입력을 최초로 받는 부분
+ */
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class DMakerController {
+    private final DMakerService dMakerService;
+
+    @GetMapping("/developers")
+    public List<String> getAllDevelopers() {
+        // Get /developers HTTP/1.1
+        log.info("Get /developers HTTP/1.1");
+
+        return Arrays.asList("snow", "Elsa", "Olaf");
+    }
+
+    @PostMapping("/create-developer")
+    public CreateDeveloper.Response createDevelopers(@Valid @RequestBody CreateDeveloper.Request request) {
+        log.info("request : {}", request);
+
+        return dMakerService.createDeveloper(request);
+
+    }
+}
